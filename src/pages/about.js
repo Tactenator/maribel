5a2bec40deb05c2134aa490a8024aeb1fb7cbeb3
Tactenator <trevor.mclaurine@gmail.com>
2023-04-04 19:10:29 -0500

    working on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,18 @@
 const About = () => {
+
+    // https://quanticalabs.com/template/cleanmate-cleaning-company-maid-gardening-template/20493947
     return ( 
         <>
+            <div id='about-container' className="">
+                <div className="">
+                    <h1>About Us</h1>
+                </div>
+                <div className="">
+                    <div id="image-container">
+                        
+                    </div>
+                </div>
+            </div>
         </>
      );
 }
