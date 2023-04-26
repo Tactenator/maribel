c8d4ad2fd81e56570c8d2984d991d4417a58a335
Tactenator <trevor.mclaurine@gmail.com>
2023-04-26 12:02:46 -0500

    added review functionality

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import LandingPage from '../images/home.jpg';
 import Cleaning from '../images/cleaning.jpg';
 import Bravo from '../images/bravologo.png'
@@ -6,10 +8,35 @@ import Mlcdc from '../images/mlcdclogog.png';
 
 
 const Home = () => {
+    const reviews = [
+        {
+        name: "Bravo Italian Kitchen", 
+        review: "Lorem Ipsum ;aksdfak;sdf;aksdf;askdflf", 
+        manager: 'Ben Reed', 
+        title: "Manager"
+    }, {
+        name: 'Mouth of the South', 
+        review: 'Is really good', 
+        manager: 'Sam',
+        title: 'Manager and Fag'
+    }, 
+    {
+        name: 'Something latino center', 
+        review: 'da best', 
+        manager: 'Some guy',
+        title: 'Manager'
+    }]
+
+    const [review, setReview] = useState(reviews[0]);
+    
+    function handleReview(e) {
+        setReview(reviews[e.target.dataset.value])
+    }
+    
     return ( 
         <>
             <div id="home-container" className='flex'>
-                <img class="w-full" src={LandingPage} alt="Comfy, clean house with blue sofa"></img>
+                <img className="w-full" src={LandingPage} alt="Comfy, clean house with blue sofa"></img>
                 <div id='landingPage-text' className='flex flex-col items-center mx-auto p-10 gap-16 font-Afterglow w-full'>
                     <h1 className='text-5xl text-sky-500 font-Monarda'>Maribel's Cleaning Service</h1>
                     <h2 className='text-3xl font-Afterglow'>Your satisfaction is our priority!</h2>
@@ -25,7 +52,7 @@ const Home = () => {
                 </div>
             </div>
             <div id='blurb-container'>
-                <div id='blurb' className='bg-sky-500 w-full flex items-center p-10 gap-12 font-Afterglow text-white'>
+                <div id='blurb' className='bg-sky-500 w-full flex items-center p-12 gap-12 font-Afterglow text-white'>
                     <div className="w-4/5 flex flex-col items-center">
                         <h1 className='text-3xl text-center'>Local, Professional and Pristine Cleaning Services for the Greater Omaha Area!</h1>
                         <h2 className='text-4xl text-center my-5'>Commercial or Residential, We Do it All!</h2>
@@ -48,22 +75,22 @@ const Home = () => {
                 <div className="text-3xl">
                     <h1 className="mb-10 font-Monarda">Brands That Trust Us</h1>
                     <div id='brands-container' className='grid grid-cols-2 gap-5'>
-                        <img src={Bravo} className="w-4/5" alt='Bravo Italian Kitchen'></img>
-                        <img src={Mots} className="w-4/5" alt="Mouth of the South Logo"></img>   
-                        <img src={Mlcdc} className="w-4/5" alt="Latino Center"></img>
+                        <img src={Bravo} data-value="0" onClick={((e) => handleReview(e))} className="w-4/5 hover:cursor-pointer transition-all duration-200
+                        hover:transform hover:translate-y-[-5px]" alt='Bravo Italian Kitchen'></img>
+                        <img src={Mots} data-value="1" onClick={((e) => handleReview(e))} className="w-4/5 hover:cursor-pointer transition-all duration-200
+                        hover:transform hover:translate-y-[-5px]" alt="Mouth of the South Logo"></img>   
+                        <img src={Mlcdc} data-value="2" onClick={((e) => handleReview(e))} className="w-4/5 hover:cursor-pointer transition-all duration-200
+                        hover:transform hover:translate-y-[-5px]" alt="Latino Center"></img>
                     </div>
                 </div>
                 <div className='w-1/2'>
                     <h1 className='text-3xl mb-10 font-Monarda'>What They're Saying</h1>
                     <div id="review" className='bg-white px-8 py-12'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum est ipsum, feugiat a aliquam ac, commodo quis dui. 
-                    Nunc magna augue, posuere quis lobortis a, tincidunt ac tellus. 
-                    Quisque sollicitudin eu nibh iaculis elementum. Duis dictum urna mi, quis lobortis odio pharetra dictum. 
-                    Integer fringilla placerat posuere.
+                    {review.review}
                     </div>
                     <div className='ml-5 mt-5'>
-                        <h3>Person Person</h3>
-                        <h3>Company Company</h3>
+                        <h3>{review.manager} - {review.title}</h3>
+                        <h3>{review.name}</h3>
                     </div>
                 </div>
             </div>
