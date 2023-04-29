52aafb3a5d16165770455bcddacc295b667927b5
Tactenator <trevor.mclaurine@gmail.com>
2023-04-28 21:11:15 -0500

    Adding modal for services

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from 'react'
+
 import Blurb from '../components/blurb';
 import Commercial from '../images/commercial.jpg'
 import Movein from '../images/movein.jpg'
@@ -7,14 +9,40 @@ import Office from '../images/office.jpg'
 import Restaurant from '../images/restaurant.jpg'
 import Contact from '../components/contact';
 
+
+
 const Services = () => {
+
+    const [data, setData] = useState();
+    const modalRef = useRef();
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+    
+    async function fetchData() { 
+        const response = await fetch("http://localhost:8000/services")
+        const data = await response.json()
+        console.log(data)
+        setData(data)
+    }
+    
+    function handleModal(e) {
+        const modal = modalRef.current
+        if(modal.style.right === '0px'){
+            modal.style.right = '-1000px'
+        }
+        else {
+            modal.style.right = '0px'
+        }
+    }
     return ( 
         <>
-            <div id="servicesBanner" className="p-16 bg-sky-400/50 text-white font-Monarda text-4xl">
+        <div id="servicesBanner" className="p-16 bg-sky-400/50 text-white font-Monarda text-4xl">
                 <h1>Our Services</h1>
             </div>
             <div id="servicesContainer" className="grid grid-cols-3 content-center justify-center place-items-center p-10">
-                <div id="service" className="w-4/5 h-auto mb-20 bg-stone-100">
+                <div id="service" data-value="0" onClick={((e) => handleModal(e))} className="w-4/5 h-auto mb-20 bg-stone-100 hover:cursor-pointer">
                     <img src={Commercial} alt="Test  for design purposes"></img>
                     <div className='px-5 pb-5'>
                         <h1 className='text-center text-2xl font-bold my-5'>Commercial Cleaning</h1>
@@ -22,7 +50,7 @@ const Services = () => {
                         <p>Scrubbing, vacuuming, you name it. We're here for all your basic cleaning needs.  </p>
                     </div>
                 </div>
-                <div id="service" className="w-4/5 h-auto mb-20 bg-stone-100">
+                <div id="service" data-value="1" onClick={((e) => handleModal(e))} className="w-4/5 h-auto mb-20 bg-stone-100 hover:cursor-pointer">
                     <img src={Movein} alt="Test  for design purposes"></img>
                     <div className='px-5 pb-5'>
                         <h1 className='text-center text-2xl font-bold my-5'>Move In/Out Service</h1>
@@ -30,15 +58,15 @@ const Services = () => {
                         <p>We'll make sure we leave the place you're leaving better than when you moved in. </p>
                     </div>
                 </div>
-                <div id="service" className="w-4/5 h-auto mb-20 bg-stone-100">
+                <div id="service" data-value="2" onClick={((e) => handleModal(e))} className="w-4/5 h-auto mb-20 bg-stone-100 hover:cursor-pointer">
                     <img src={Pressure} alt="Test  for design purposes"></img>
                     <div className='px-5 pb-5'>
-                        <h1 className='text-center text-2xl font-bold my-5'>Pressure Cleaning</h1>
+                        <h1 className='text-center text-2xl font-bold my-5 '>Pressure Cleaning</h1>
                         <div className='mx-auto bg-sky-500 mb-5 h-[3px] w-[50px]'></div>
                         <p>Need something a little more heavy duty? We got you covered.  </p>
                     </div>
                 </div>
-                <div id="service" className="w-4/5 h-auto mb-20 bg-stone-100">
+                <div id="service" data-value="3" onClick={((e) => handleModal(e))} className="w-4/5 h-auto mb-20 bg-stone-100 hover:cursor-pointer">
                     <img src={After} alt="Test for design purposes"></img>
                     <div className='px-5 pb-5'>
                         <h1 className='text-center text-2xl font-bold my-5'>After Hours Cleaning</h1>
@@ -46,7 +74,7 @@ const Services = () => {
                         <p>Closing up shop? We'll take care of all your end of the day cleaning needs.  </p>
                     </div>
                 </div>
-                <div id="service" className="w-4/5 h-auto mb-20 bg-stone-100">
+                <div id="service" data-value="4" onClick={((e) => handleModal(e))} className="w-4/5 h-auto mb-20 bg-stone-100 hover:cursor-pointer">
                     <img src={Office} alt="Test for design purposes"></img>
                     <div className='px-5 pb-5'>
                         <h1 className='text-center text-2xl font-bold my-5'>Office Cleaning</h1>
@@ -54,7 +82,7 @@ const Services = () => {
                         <p>We keep offices nice and tidy so you can focus more on getting the job done. </p>
                     </div>
                 </div>
-                <div id="service" className="w-4/5 h-auto mb-20 bg-stone-100">
+                <div id="service" data-value="5" onClick={((e) => handleModal(e))} className="w-4/5 h-auto mb-20 bg-stone-100 hover:cursor-pointer">
                     <img src={Restaurant} alt="Test for design purposes"></img>
                     <div className='px-5 pb-5'>
                         <h1 className='text-center text-2xl font-bold my-5'>Restaurant Services</h1>
@@ -63,9 +91,15 @@ const Services = () => {
                     </div>
                 </div>
             </div>
+            {data && 
+                <div id="modal" ref={modalRef} style={{right: '-1000px'}} className='absolute top-0  h-[100vh] w-1/3 bg-white transition-all duration-300'>
+                    {data[0].name}
+                </div>
+            }
             <Blurb />
             <Contact />
         </>
+            
      );
 }
  
