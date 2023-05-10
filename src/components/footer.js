f99966278dbd44e6f8117a2df4d270e43be3c697
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-05-09 21:48:25 -0500

    working on mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,26 +5,35 @@ const Footer = () => {
         <footer className="flex justify-evenly bg-sky-500 pt-10 pb-32 px-20 text-white font-Afterglow text-2xl">
             <div>
                 <h1 className='mb-5 font-bold font-Monarda text-3xl'>Maribel's Cleaning Service</h1>
-                <ul className="flex flex-col gap-2">
-                <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/">Home</Link>
-                <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/about">About</Link>
-                <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/services">Services</Link>
-                <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/contact">Contact</Link>
-            </ul>
+                <ul className="flex flex-col gap-2 ml-5">
+                    <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/">Home</Link>
+                    <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/about">About</Link>
+                    <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/services">Services</Link>
+                    <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/contact">Contact</Link>
+                </ul>
             </div>
-            <div className="flex flex-row justify-evenly gap-20">
+            <div className="flex flex-row justify-evenly gap-32">
                 <div>
-                    <h2 className='font-bold text-2xl mb-10'>Working Hours</h2>
+                    <h2 className='font-bold text-2xl mb-10 underline'>Working Hours</h2>
+                    <ul>
+                        <li className="mb-5">Weekdays: 8AM - 5PM</li>
+                        <li className="">Weekdays: 8AM - 5PM</li>
+                    </ul>
                 </div>
                 <div>
-                    <h2 className='font-bold text-2xl mb-10'>Contact Information</h2>
+                    <h2 className='font-bold text-2xl mb-10 underline'>Contact Information</h2>
                     <ul>
                         <li className='mb-5'>Phone: <a href="tel:4022037491" className="font-Afterglow hover:underline">402-203-7491</a></li>
                         <li>Email: <a href='mailto: cleaning1now@gmail.com' className='font-Afterglow hover:underline'>cleaning1now@gmail.com</a></li>
                     </ul>
                 </div>
                 <div>
-                    <h2 className='font-bold text-2xl mb-10'>Payment Accepted</h2>
+                    <h2 className='font-bold text-2xl mb-10 underline'>Payment Accepted*</h2>
+                    <ul>
+                        <li className="mb-5">Cash</li>
+                        <li className="mb-5">Checks</li>
+                        <li className='text-sm'>*No Apple Pay or Venmo at this time</li>
+                    </ul>
                 </div>
             </div>
         </footer>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,46 @@
 import { Link } from "react-router-dom";
+import { useRef } from 'react';
 
 const Navbar = () => {
+    const mobileRef = useRef()
+
+    function handleMobile(e) {
+        e.preventDefault()
+        
+        if(mobileRef.current.style.display === 'none'){
+            mobileRef.current.style.display = 'block'
+        }
+        else {
+            mobileRef.current.style.display = 'none'
+        }
+    }
     return ( 
         <>
-        <nav className="flex justify-between items-center pt-6 pb-6 pr-24 pl-24 bg-sky-500 text-white">
-            <h1 className="text-3xl font-Monarda">Maribel's Cleaning Service</h1>
-            <h3 className="text-2xl font-Afterglow">Omaha, NE</h3>
-            <a href="tel:4022037491" className="text-2xl font-Afterglow hover:underline">402-203-7491</a>
-            <ul className="flex gap-7">
-                <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/">Home</Link>
-                <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/about">About</Link>
-                <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/services">Services</Link>
-                <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/contact">Contact</Link>
-            </ul>
-
-        </nav>
+            <nav className="flex justify-around items-center pt-6 pb-6 bg-sky-500 text-white md:pl-24 md:pr-24 md:gap-10">
+                <h1 className="sm:block text-3xl font-Monarda">Maribel's Cleaning Service</h1>
+                <h3 className="hidden lg:block text-2xl font-Afterglow">Omaha, NE</h3>
+                <a href="tel:4022037491" className="hidden lg:block text-2xl font-Afterglow hover:underline">402-203-7491</a>
+                <ul className="hidden md:flex gap-7">
+                    <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/">Home</Link>
+                    <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/about">About</Link>
+                    <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/services">Services</Link>
+                    <Link className="text-2xl font-Afterglow transition-all duration-200 hover:underline" to="/contact">Contact</Link>
+                </ul>
+                <div  id="hamburger-menu" className="md:hidden" onClick={handleMobile}>
+                    <div className='mx-auto bg-white mb-1 h-[3px] w-[25px] pointer-events-none'></div>
+                    <div className='mx-auto bg-white mb-1 h-[3px] w-[25px] pointer-events-none'></div>
+                    <div className='mx-auto bg-white mb-1 h-[3px] w-[25px] pointer-events-none'></div>
+                </div>
+                <div ref={mobileRef} id="mobileMenu" className="absolute top-0 left-0 h-[100vh] w-[100vw] z-20 bg-sky-500" style={{display: 'none'}}>
+                    <h1 className="sm:block text-3xl font-Monarda text-center mb-20 mt-10">Maribel's Cleaning Service</h1>
+                    <ul className="flex flex-col items-center gap-7">
+                        <Link className="text-4xl font-Afterglow transition-all duration-200 hover:underline" to="/">Home</Link>
+                        <Link className="text-4xl font-Afterglow transition-all duration-200 hover:underline" to="/about">About</Link>
+                        <Link className="text-4xl font-Afterglow transition-all duration-200 hover:underline" to="/services">Services</Link>
+                        <Link className="text-4xl font-Afterglow transition-all duration-200 hover:underline" to="/contact">Contact</Link>
+                    </ul>
+                </div>
+            </nav>
         </>
      );
 }
